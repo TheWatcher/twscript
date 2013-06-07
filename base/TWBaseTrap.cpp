cde4911c85c894a135b831d0b9a6c4ee74209f38
Chris <chris@starforge.co.uk>
2013-06-07 22:05:16 +0100

    Debugging output during init.

--- base/TWBaseTrap.cpp
+++ base/TWBaseTrap.cpp
@@ -67,6 +67,9 @@ void TWBaseTrap::init(int time)
             g_pMalloc -> Free(msg);
         }
 
+        if(debug_enabled())
+            debug_printf(DL_DEBUG, "Trap initialised with on = '%s', off = '%s'", static_cast<const char *>(turnon_msg), static_cast<const char *>(turnoff_msg));
+
         // Now for use limiting.
         int value, falloff;
         get_scriptparam_valuefalloff(design_note, "Count", &value, &falloff);
@@ -75,12 +78,21 @@ void TWBaseTrap::init(int time)
         // Handle modes
         count_mode = get_scriptparam_countmode(design_note, "CountOnly");
 
+        if(debug_enabled())
+            debug_printf(DL_DEBUG, "Count is %d %s with a falloff of %d milliseconds, count mode is %d", value, (value ? "" : "(no use limit)"), falloff, static_cast<int>(count_mode));
+
         // Now deal with capacitors
         get_scriptparam_valuefalloff(design_note, "OnCapacitor", &value, &falloff);
         on_capacitor.init(time, value, 0, falloff);
 
+        if(debug_enabled())
+            debug_printf(DL_DEBUG, "OnCapacitor is %d %s with a falloff of %d milliseconds", value, (value > 1 ? "" : "(every turnon fires)"), falloff);
+
         get_scriptparam_valuefalloff(design_note, "OffCapacitor", &value, &falloff);
         off_capacitor.init(time, value, 0, falloff);
+
+        if(debug_enabled())
+            debug_printf(DL_DEBUG, "OffCapacitor is %d %s with a falloff of %d milliseconds", value, (value > 1 ? "" : "(every turnoff fires)"), falloff);
 
         g_pMalloc -> Free(design_note);
     }
